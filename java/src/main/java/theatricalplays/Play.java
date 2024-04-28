6da58fa4633c945447339f5afb988dff201c5c3f
Laurent Richard <laurent.richard@harvest.fr>
2024-04-28 23:25:05 +0200

    refactor: move to Play

--- java/src/main/java/theatricalplays/Play.java
+++ java/src/main/java/theatricalplays/Play.java
@@ -1,5 +1,55 @@
 package theatricalplays;
 
+import java.util.Map;
+
 public record Play (String name, Type type) {
-    enum Type { TRAGEDY, COMEDY, WESTERN }
+
+    enum Type { TRAGEDY, COMEDY, WESTERN;
+
+        private static final Map<Type, PricingStrategy> PRICING_STRATEGIES = Map.of(
+                COMEDY, new ComedyPricingStrategy(),
+                TRAGEDY, new TragegyPricingStrategy()
+        );
+
+        interface PricingStrategy {
+            int calculateAmount(int audience);
+        }
+
+        static class ComedyPricingStrategy implements PricingStrategy {
+            @Override
+            public int calculateAmount(int audience) {
+                int amount = 30000 + 300 * audience;
+                if (audience > 20) {
+                    amount += 10000 + 500 * (audience - 20);
+                }
+                return amount;
+            }
+        }
+
+        static class TragegyPricingStrategy implements PricingStrategy {
+            @Override
+            public int calculateAmount(int audience) {
+                int amount = 40000;
+                if (audience > 30) {
+                    amount += 1000 * (audience - 30);
+                }
+                return amount;
+            }
+        }
+        int calculateAmount(int audience) {
+            PricingStrategy strategy = PRICING_STRATEGIES.get(this);
+            if (strategy == null) {
+                throw new IllegalArgumentException("unknown type: " + this);
+            }
+            return strategy.calculateAmount(audience);
+        }
+
+        int calculateVolumeCredits(int audience) {
+            int volumeCredits = Math.max(audience - 30, 0);
+            // add extra credit for every five comedy attendees
+            if (this == COMEDY) volumeCredits += audience / 5;
+            return volumeCredits;
+        }
+    }
+
 }
--- java/src/main/java/theatricalplays/StatementData.java
+++ java/src/main/java/theatricalplays/StatementData.java
@@ -3,8 +3,6 @@ package theatricalplays;
 import java.util.List;
 import java.util.Map;
 
-import static theatricalplays.Play.Type.*;
-
 public class StatementData {
 
     final String customer;
@@ -22,61 +20,17 @@ public class StatementData {
     }
 
     static class PerformanceData {
-        interface PricingStrategy {
-            int calculateAmount(int audience);
-        }
-        private static final Map<Play.Type, PricingStrategy> PRICING_STRATEGIES = Map.of(
-                COMEDY, new ComedyPricingStrategy(),
-                TRAGEDY, new TragegyPricingStrategy()
-        );
 
         final String playName;
-        final Play.Type playType;
         final int audience;
         final int amount;
         final int volumeCredits;
 
-        static class ComedyPricingStrategy implements PricingStrategy {
-            @Override
-            public int calculateAmount(int audience) {
-                int amount = 30000 + 300 * audience;
-                if (audience > 20) {
-                    amount += 10000 + 500 * (audience - 20);
-                }
-                return amount;
-            }
-        }
-        static class TragegyPricingStrategy implements PricingStrategy {
-            @Override
-            public int calculateAmount(int audience) {
-                int amount = 40000;
-                if (audience > 30) {
-                    amount += 1000 * (audience - 30);
-                }
-                return amount;
-            }
-        }
         PerformanceData(Play play, int audience) {
             this.playName = play.name();
-            this.playType = play.type();
             this.audience = audience;
-            this.amount = calculateAmount();
-            this.volumeCredits = calculateVolumeCredits();
-        }
-
-        private int calculateAmount() {
-            PricingStrategy strategy = PRICING_STRATEGIES.get(playType);
-            if (strategy == null) {
-                throw new IllegalArgumentException("unknown type: " + playType);
-            }
-            return strategy.calculateAmount(audience);
-        }
-
-        private int calculateVolumeCredits() {
-            int volumeCredits = Math.max(audience - 30, 0);
-            // add extra credit for every five comedy attendees
-            if (playType==COMEDY) volumeCredits += audience / 5;
-            return volumeCredits;
+            this.amount = play.type().calculateAmount(audience);
+            this.volumeCredits = play.type().calculateVolumeCredits(audience);
         }
 
     }
