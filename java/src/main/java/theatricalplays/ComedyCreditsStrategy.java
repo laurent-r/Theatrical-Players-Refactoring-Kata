514844c7bfbdaae5634bf6204b08de18c369d093
Laurent Richard <laurent.richard@harvest.fr>
2024-06-23 23:16:41 +0200

    refactor: adjust credits strategy

--- java/src/main/java/theatricalplays/ComedyCreditsStrategy.java
+++ java/src/main/java/theatricalplays/ComedyCreditsStrategy.java
@@ -3,10 +3,8 @@ package theatricalplays;
 class ComedyCreditsStrategy extends CreditsStrategy {
 
     @Override
-    public int calculate(int audience) {
-        int volumeCredits = super.calculate(audience);
+    protected int extraCredits(int audience) {
         // add extra credit for every five comedy attendees
-        volumeCredits += audience / 5;
-        return volumeCredits;
+        return audience / 5;
     }
 }
--- java/src/main/java/theatricalplays/CreditsStrategy.java
+++ java/src/main/java/theatricalplays/CreditsStrategy.java
@@ -3,6 +3,15 @@ package theatricalplays;
 class CreditsStrategy {
 
     int calculate(int audience) {
+        return baseCredits(audience) + extraCredits(audience);
+    }
+
+    protected int baseCredits(int audience) {
         return Math.max(audience - 30, 0);
     }
+
+    protected int extraCredits(int audience) {
+        return 0;
+    }
+
 }
